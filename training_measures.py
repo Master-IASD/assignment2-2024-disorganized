bd7c2a881f5fa6e042cda6d169c04b1a08044915
BarthelemyBregeon <131197562+BarthelemyBregeon@users.noreply.github.com>
2024-11-11 16:21:30 +0100

    training and testing stuff

--- training_measures.py
+++ training_measures.py
@@ -8,14 +8,14 @@ n_epochs = 0
 
 subprocess.run(['python','train.py']+['--epochs=10'])
 n_epochs += 10
-"""subprocess.run(['python','generate_baseline.py'])
+subprocess.run(['python','generate_baseline.py'])
 
 precision, recall = precision_recall.compute_precision_recall('samples_baseline')
    
 with open('training_measures.txt','a') as file :
     file.write(f"Number of epochs : {n_epochs}\n")
     file.write(f"precision : {precision}\n")
-    file.write(f"recall : {recall}\n")"""
+    file.write(f"recall : {recall}\n")
         
 for i in range(100):
 
@@ -29,4 +29,3 @@ for i in range(100):
         file.write(f"Number of epochs : {n_epochs}\n")
         file.write(f"precision : {precision}\n")
         file.write(f"recall : {recall}\n")
-        #file.write(f"fid : {fid}\n")
